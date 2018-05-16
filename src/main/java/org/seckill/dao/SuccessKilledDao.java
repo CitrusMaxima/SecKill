6afcc5ed6b93cfb3be48783aadc60bb0e76b6820
CitrusMaxima <2663753900@qq.com>
2018-05-16 21:01:59 +0800

    pojo层和dao层接口开发

---
+++ src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -0,0 +1,25 @@
+package org.seckill.dao;
+
+import org.seckill.pojo.SuccessKilled;
+
+/**
+ * Created by CitrusMaxima on 2018/5/16.
+ */
+public interface SuccessKilledDao {
+
+    /**
+     * 插入购买明细，可过滤重复
+     * @param seckillId
+     * @param userPhone
+     * @return 插入的行数
+     */
+    int insertSuccessKilled(long seckillId, long userPhone);
+
+    /**
+     * 根据id查询SuccessKilled并携带秒杀产品对象
+     * @param seckillId
+     * @return
+     */
+    SuccessKilled queryByIdWithSeckill(long seckillId);
+
+}
